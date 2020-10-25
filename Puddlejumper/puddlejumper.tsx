da45b77854d49b0acc88da248a358d60e84d3ab9
Chris Goodwin <chris@goodwinillustration.com>
2020-10-25 19:51:47 -0400

    added baddies

---
+++ Puddlejumper/puddlejumper.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="puddlejumper" tilewidth="64" tileheight="64" tilecount="1" columns="0">
+ <editorsettings>
+  <export target="puddlejumper..tsx" format="tsx"/>
+ </editorsettings>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <terraintypes>
+  <terrain name="tile01" tile="-1"/>
+ </terraintypes>
+ <tile id="0">
+  <image width="64" height="64" source="blocks/tile01.png"/>
+ </tile>
+</tileset>
