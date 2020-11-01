61ff63421a81ac2a7d55973313670c663d49a5ef
Chris Goodwin <chris@goodwinillustration.com>
2020-10-31 23:45:04 -0400

    upfdate

--- Puddlejumper/tiles/test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="test" tilewidth="64" tileheight="64" tilecount="264" columns="22">
- <image source="tilemapPuddlejumper.png" width="1408" height="768"/>
-</tileset>
