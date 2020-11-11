3aa3433f0034424cfc96eec7aff9a09ce4e274c1
Chris Goodwin <chris@goodwinillustration.com>
2020-11-11 16:55:49 -0500

    udpate

---
+++ Puddlejumper/scenes/templateTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="templateTiles" tilewidth="64" tileheight="64" tilecount="264" columns="22">
+ <image source="../tiles/templateTiles.png" width="1408" height="768"/>
+</tileset>
