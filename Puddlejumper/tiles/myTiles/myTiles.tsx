81db8a6e879ef5750f69676d4864f3761bd80ca1
Chris Goodwin <chris@goodwinillustration.com>
2023-09-23 15:39:41 -0400

    updating stufff

---
+++ Puddlejumper/tiles/myTiles/myTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="myTiles" tilewidth="64" tileheight="64" tilecount="216" columns="27">
+ <image source="../../scenes/tiles00.png" width="1728" height="512"/>
+</tileset>
