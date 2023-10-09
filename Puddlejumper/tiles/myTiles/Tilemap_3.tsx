f54927104d6619da4b71ba2800fe57c13f28ae59
Chris Goodwin <chris@goodwinillustration.com>
2023-10-08 21:00:57 -0400

    UPPY DATIE

---
+++ Puddlejumper/tiles/myTiles/Tilemap_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Tilemap_3" tilewidth="32" tileheight="32" tilecount="1056" columns="44">
+ <image source="Tilemap_3.png" width="1408" height="768"/>
+</tileset>
