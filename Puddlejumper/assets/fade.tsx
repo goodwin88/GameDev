16382966f3c90024234cf7ce2816e54c3240edcd
Chris Goodwin <chris@goodwinillustration.com>
2024-02-10 10:54:53 -0500

    update

--- Puddlejumper/assets/fade.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="fade" tilewidth="64" tileheight="64" tilecount="20" columns="5">
- <image source="fade.png" width="320" height="256"/>
-</tileset>
--- Puddlejumper/assets/myTilesForReal2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="myTilesForReal2" tilewidth="64" tileheight="64" tilecount="160" columns="20">
- <image source="myTilesForReal2.png" width="1280" height="575"/>
-</tileset>
